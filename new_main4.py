99e84bd17ca4da05511dedf33f91e432d93533eb
liucu0135 <darkcoke2005@163.com>
2020-10-28 02:08:57 +0800

    attent_norm, norm_false

--- new_main4.py
+++ new_main4.py
@@ -42,8 +42,8 @@ if __name__ == '__main__':
     args.use_BN=False
     # args.retrain = "data/checkpoints/legacy.pth.tar"
     # args.init_lr=1e-5
-    args.lr_decay=0.5
-    args.batch=32
+    args.lr_decay=0.2
+    args.batch=64
     args.in_img_num=32
     args.val_batch=16
     args.save_root= 'data/checkpoints/atten_norm'
